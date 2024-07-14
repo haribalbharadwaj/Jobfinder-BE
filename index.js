7d4820f6dc73306eb8629b9015a366388214a99e
BHARADWAJ HARIBAL <haribalbharadwaj@gmail.com>
2024-07-14 16:31:03 +0530

    4th commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const userRoutes = require('./src/routes/user')
+const jobRoutes = require('./src/routes/job')
+const errorHandler = require('./src/middleware/errorHandler')
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+app.use(userRoutes)
+app.use(jobRoutes)
+app.use(errorHandler);
+
+app.get('/',(req,res)=>{
+    res.send({
+        status:"Server is up",
+        now:new Date().toLocaleDateString()
+    })
+})
+
+app.listen(process.env.PORT,()=>{
+    mongoose
+        .connect(process.env.MONGODB_URL)
+        .then(()=> console.log('Server is running'))
+        .catch((error)=> console.log(error))
+})

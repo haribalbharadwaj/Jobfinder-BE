b11a59c10bf8aaddb41f59f25ba57ee79c7e5caa
BHARADWAJ HARIBAL <haribalbharadwaj@gmail.com>
2024-07-15 15:57:36 +0530

    perfect token

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,40 +1,18 @@
 const jwt = require('jsonwebtoken');
-const {JWT_Private_Key} = process.env;
 
 
 const verifyToken = (req, res, next) => {
+
     try {
-        const authHeader = req.header('Authorization');
-        if (!authHeader) {
-            console.log('Authorization header missing');
-            return res.status(401).json({
-                message: 'Authorization header missing'
-            });
-        }
-        console.log('Auth Header:', authHeader); // Log the auth header
+        const token = req.header('Authorization').split(' ')[1];
+        if (!token) return res.status(401).json({ message: 'Token Not Found or Valid' });
 
-        const token = authHeader.split(' ')[1];
-
-        console.log('Token:', token); // Log the token
-
-        if (!token) {
-            console.log('Token not found');
-            return res.status(401).json({
-                message: 'Token not found or valid'
-            });
-        }
-
-        const decoded = jwt.verify(token, JWT_Private_Key);
-
-        console.log('Decoded Token:', decoded); // Log the decoded token
-
-        req.userID = decoded.userID; // Correctly set the userID
+        const decoded = jwt.verify(token, 'secret');
+        console.log(decoded);
+        req.refUserId = decoded.userID;
         next();
     } catch (error) {
-        console.log('Token verification error:', error.message); // Log any error
-        return res.status(401).json({
-            message: 'Token not found or valid'
-        });
+        return res.status(401).json({ message: 'Token Not Found or Valid' });
     }
 };
 
